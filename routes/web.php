ab09b7462d5808b79e9b17b8de47d5471a86f856
t-takumi-h <t.takumi.h@gmail.com>
2021-08-03 08:13:28 +0900

    chapter05まで作成

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+
+Route::post('/folders/create', 'FolderController@create');
