5c53d7e9c950f6661dbd96eafbc413d930ba2857
t-takumi-h <t.takumi.h@gmail.com>
2021-08-05 19:33:42 +0900

    chapter06まで作成

--- config/database.php
+++ config/database.php
@@ -43,6 +43,26 @@ return [
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
 
+        'test_laravel_task' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => '/Applications/MAMP/tmp/mysql/mysql.sock',
+            'charset' => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
--- database/migrations/2021_07_30_070458_create_tasks_table.php
+++ database/migrations/2021_07_30_070458_create_tasks_table.php
@@ -22,7 +22,7 @@ class CreateTasksTable extends Migration
             $table->timestamps();
             
             //外部キーを設定する
-            $table->foreing('folder_id')->references('id')->on('folders');
+            $table->foreign('folder_id')->references('id')->on('folders');
         });
     }
 
